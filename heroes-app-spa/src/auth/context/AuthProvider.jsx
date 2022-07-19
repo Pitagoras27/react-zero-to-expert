07bcde9b2d4901be79b0ae5184f343af313e1134
Carlos Gutiérrez <elimperiodeloefimero@gmail.com>
2022-07-19 18:55:22 -0500

    feature: add private route and public route

---
+++ heroes-app-spa/src/auth/context/AuthProvider.jsx
@@ -0,0 +1,69 @@
+import { useReducer } from "react";
+import { types } from "../../types/types";
+import { AuthContext } from "./AuthContext";
+import { authReducer } from "./authReducer";
+
+// const initialState = {
+//   logged: false,
+// };
+
+// const init = () => {
+//   const user = JSON.parse(localStorage.getItem("user"));
+//   return {
+//     logged: !!user,
+//     name: user,
+//   };
+// };
+
+const init = () => {
+  const user = JSON.parse(localStorage.getItem("user"));
+
+  return {
+    logged: !!user,
+    user,
+  };
+};
+
+export const AuthProvider = ({ children }) => {
+  const [stateReducer, dispatch] = useReducer(authReducer, {}, init);
+
+  const login = ({ user }) => {
+    const dataUser = {
+      id: "abc",
+      name: user,
+    };
+    // ? It's important save data with JSON.stringify because to retrive in getItem of localstorage
+    // ? use JSON.parse. Otherway it don't work.
+    localStorage.setItem("user", JSON.stringify(dataUser));
+
+    const actionType = {
+      type: types.login,
+      payload: dataUser,
+    };
+
+    dispatch(actionType);
+  };
+
+  const logout = () => {
+    const actionType = {
+      type: types.logout,
+      payload: null,
+    };
+
+    localStorage.removeItem("user");
+
+    dispatch(actionType);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        ...stateReducer,
+        login,
+        logout,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
--- heroes-app-spa/src/ui/components/Navbar.jsx
+++ heroes-app-spa/src/ui/components/Navbar.jsx
@@ -1,13 +1,18 @@
+import { useContext } from "react";
 import { Link, NavLink, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../auth/context/AuthContext";
 
 export const Navbar = () => {
   const navigate = useNavigate();
 
+  const { user, logout } = useContext(AuthContext);
   const onLogout = () => {
+    logout();
     // ? redirecciona, y mediante el segundo argumento indica que se debe eliminar del stack de navegacion del navegador
     // ? la pagina desde la cual se presionó el boton logout
     navigate("/login", { replace: true });
   };
+
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
       <Link className="navbar-brand" to="/">
@@ -46,7 +51,7 @@ export const Navbar = () => {
 
       <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 justify-content-end">
         <ul className="navbar-nav ml-auto">
-          <span className="nav-item nav-link text-primary">Carlos</span>
+          <span className="nav-item nav-link text-primary">{user?.name}</span>
           <button onClick={onLogout} className="nav-item nav-link btn">
             Logout
           </button>
