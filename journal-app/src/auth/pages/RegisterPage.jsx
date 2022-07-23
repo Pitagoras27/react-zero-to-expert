b7f48149256b7ec88d727155cb6aa44986d60668
Carlos Gutiérrez <elimperiodeloefimero@gmail.com>
2022-07-22 22:01:05 -0500

    feature: Create Layout, apply DRY principle

--- journal-app/src/auth/pages/RegisterPage.jsx
+++ journal-app/src/auth/pages/RegisterPage.jsx
@@ -1,3 +1,62 @@
+import { Google } from "@mui/icons-material";
+import { Button, Grid, Link, TextField, Typography } from "@mui/material";
+import { Link as RouterLink } from "react-router-dom";
+import { AuthLayout } from "../layout/AuthLayout";
+
 export const RegisterPage = () => {
-  return <h1>RegisterPage</h1>;
+  return (
+    <AuthLayout title="Register">
+      <form>
+        <Grid container>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Full Name"
+              type="text"
+              placeholder="Full Name"
+              autoComplete="off"
+              fullWidth
+            />
+          </Grid>
+        </Grid>
+
+        <Grid container>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Correo"
+              type="email"
+              placeholder="correo@google.com"
+              autoComplete="off"
+              fullWidth
+            />
+          </Grid>
+        </Grid>
+
+        <Grid container spacing={2} sx={{ mb: 2 }}>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Password"
+              type="password"
+              placeholder="Password"
+              autoComplete="off"
+              fullWidth
+            />
+          </Grid>
+        </Grid>
+
+        <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
+          <Grid item xs={12} sm={6}>
+            <Button variant="contained" fullWidth>
+              <Typography>Register</Typography>
+            </Button>
+          </Grid>
+
+          <Grid container direction="row" justifyContent="end">
+            <Link component={RouterLink} color="inherit" to="/auth/login">
+              <Typography sx={{ mt: 2 }}>You have an Account</Typography>
+            </Link>
+          </Grid>
+        </Grid>
+      </form>
+    </AuthLayout>
+  );
 };
