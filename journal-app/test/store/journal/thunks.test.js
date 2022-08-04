f17ac10593b698746b4fd7bc2c6e9499d669bebb
Carlos Gutiérrez <elimperiodeloefimero@gmail.com>
2022-08-04 14:58:17 -0500

    test: Clean notes of instance DB for firebase and add expects of thunk startNewNote

--- journal-app/test/store/journal/thunks.test.js
+++ journal-app/test/store/journal/thunks.test.js
@@ -1,4 +1,11 @@
-import { startNewNote } from "../../../src/store/journal";
+import { collection, deleteDoc, getDocs } from "firebase/firestore/lite";
+import { FirebaseDB } from "../../../src/firebase/config";
+import {
+  addNewEmptyNote,
+  savingNote,
+  setActiveNote,
+} from "../../../src/store/journal/journalSlide";
+import { startNewNote } from "../../../src/store/journal/thunks";
 
 describe("test for thunks of journal", () => {
   const dispatch = jest.fn();
@@ -8,8 +15,30 @@ describe("test for thunks of journal", () => {
 
   test("should call startNewNote and create new note", async () => {
     const uid = "test-uid";
+    const noteData = {
+      title: "",
+      description: "",
+      imageUrls: [],
+      date: expect.any(Number),
+      id: expect.any(String),
+    };
+
     getState.mockReturnValue({ auth: { uid } });
 
     await startNewNote()(dispatch, getState);
+
+    expect(dispatch).toHaveBeenCalledWith(savingNote());
+    expect(dispatch).toHaveBeenCalledWith(addNewEmptyNote(noteData));
+    expect(dispatch).toHaveBeenCalledWith(setActiveNote(noteData));
+
+    // Clean instance DB test of firebase
+    const collectionRef = collection(FirebaseDB, `${uid}/journal/notes`);
+    // ! docs is not array = Array.isArray(docs) // false
+    // ! nevertheless iterate docs with forEach
+    const docs = await getDocs(collectionRef);
+
+    const promisesToDelete = [];
+    docs.forEach((doc) => promisesToDelete.push(deleteDoc(doc.ref)));
+    await Promise.all(promisesToDelete);
   });
 });
