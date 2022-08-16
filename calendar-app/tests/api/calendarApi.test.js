56edfed6c906cbf94e6e163df000696be8f510b3
Carlos Gutiérrez <elimperiodeloefimero@gmail.com>
2022-08-16 15:06:22 -0500

    feat: test of calendarApi

--- calendar-app/tests/api/calendarApi.test.js
+++ calendar-app/tests/api/calendarApi.test.js
@@ -1,3 +1,24 @@
-// should default config
+import calendarApi from "../../src/api/calendarApi";
 
-// should send header `x-token` in request
+describe('test calendar API', () => {
+
+  test('should default config', async() => {
+    const envVar = process.env.VITE_BASE_API_CALENDAR;
+    expect(calendarApi.defaults.baseURL).toBe(envVar);
+  });
+
+  test('should send header `x-token` in any request', async () => {
+    try {
+      const token = 'ABC-xyz-123-987'
+      localStorage.setItem('token', token)
+  
+      const res = await calendarApi.get('/auth')
+  
+      expect(res.config.headers['x-token']).toBe(token);  
+    } catch (error) {
+      console.log(error)
+    }
+  })
+
+
+})
