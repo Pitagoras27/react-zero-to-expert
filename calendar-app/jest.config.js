e761bc01359947d2f608b3045cbbd5bf1bbf586a
Carlos Gutiérrez <elimperiodeloefimero@gmail.com>
2022-08-18 15:05:12 -0500

    feat: Add cofig to run css code in suite test of library testing

--- calendar-app/jest.config.js
+++ calendar-app/jest.config.js
@@ -1,10 +1,9 @@
 module.exports = {
   testEnvironment: 'jest-environment-jsdom',
   setupFiles: ['./jest.setup.js'],
-  transformIgnorePatterns: []
+  transformIgnorePatterns: [],
   // ModuleNameMapper sólo si ocupamos importar CSS en nuestros componentes para el testing
-  // !Uncomment in its time
-  // moduleNameMapper: {
-  //     '\\.(css|less)$': '<rootDir>/tests/mocks/styleMock.js',
-  // },
+  moduleNameMapper: {
+      '\\.(css|less)$': '<rootDir>/tests/mocks/styleMock.js',
+  },
 }
